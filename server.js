137f720e7f94cff676d8cc452bf2719525d5eaea
german <germanevangelisti60559@gmail.com>
2018-10-23 21:35:54 -0300

    probando path

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/nurvey'));
+app.use(express.static('./dist/nurvey-primeng'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname,'/dist/nurvey/index.html'));
+res.sendFile(path.join(__dirname,'/dist/nurvey-primeng/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
