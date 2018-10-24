b5dc915abdec05ed7e5831b10f0dfefb2d82beca
german <germanevangelisti60559@gmail.com>
2018-10-23 21:45:49 -0300

    probando path

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/nurvey-primeng'));
-
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname,'/dist/nurvey-primeng/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
+// //Install express server
 // const express = require('express');
+// const path = require('path');
+
 // const app = express();
 
-// // Run the app by serving the static files in the dist directory
-// app.use(express.static(__dirname + '/dist'));
+// // Serve only the static files form the dist directory
+// app.use(express.static('./dist/nurvey-primeng'));
+
+// app.get('/*', function(req,res) {
+
+// res.sendFile(path.join(__dirname,'/dist/nurvey-primeng/index.html'));
+// });
 
 // // Start the app by listening on the default Heroku port
 // app.listen(process.env.PORT || 8080);
+
+const express = require('express');
+const app = express();
+
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist/nurvey-primeng'));
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
