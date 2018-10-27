adeb60174aa2ab5e195684f48078090b162d6122
Jeremias <jerecabrera16@gmail.com>
2018-10-27 11:33:06 -0300

    probando el path

--- server.js
+++ server.js
@@ -18,6 +18,10 @@
 const express = require('express');
 const app = express();
 
+app.get('*', function (req, res) {
+    res.sendfile('./src/index.html')
+})
+
 // Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
