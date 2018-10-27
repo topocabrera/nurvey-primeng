aed2a9b29b5ae5c4fb319496e97f9dc922672e2d
Jeremias <jerecabrera16@gmail.com>
2018-10-27 11:52:32 -0300

    prueba path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // //Install express server
 // const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 // const app = express();
 
@@ -18,15 +18,12 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-// app.get('*', function (req, res) {
-//     res.sendfile('./src/index.html')
-// })
+app.get('*', function (req, res) {
+    res.sendfile('/dist')
+})
 
 // Run the app by serving the static files in the dist directory
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist'));
-});
-//app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
