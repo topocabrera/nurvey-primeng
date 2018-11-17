d202430d38d5e336306f852f9cf83c5c177aa4df
Jeremias <jerecabrera16@gmail.com>
2018-11-17 18:25:12 -0300

    add styles and logic for mobile device and desktop

--- src/app/components/misencuestas/misencuestas.component.ts
+++ src/app/components/misencuestas/misencuestas.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute} from '@angular/router'
+import { Router, ActivatedRoute } from '@angular/router'
 import * as Survey from 'survey-angular';
 import * as SurveyEditor from 'surveyjs-editor';
 import { SurveyService } from './../../services/survey.service';
 import { SurveyModelClass } from './../../domain/SurveyModelClass';
 import { AlertService } from './../../services/index';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
-declare var $:any;
+declare var $: any;
 declare var require: any
 declare let routerAlert: Router;
 
-var CryptoJS = require("crypto-js");
+const CryptoJS = require('crypto-js');
 
 @Component({
     selector: 'misEncuestas',
@@ -21,166 +22,169 @@ var CryptoJS = require("crypto-js");
 
 export class misEncuestasComponent implements OnInit {
     surveyRender: object;
-    encuestas:SurveyModelClass[] = [];
+    encuestas: SurveyModelClass[] = [];
     activeEncuesta;
     encuestaActiva: boolean;
     surveyService: SurveyService;
     testSurveyModel: SurveyEditor.SurveyEditor;
-    currentUser:any = JSON.parse(localStorage.getItem('currentUser'));
-    loading:boolean;
-    mensajeAlert:string;
-    estiloAlert:string;
+    currentUser: any = JSON.parse(localStorage.getItem('currentUser'));
+    loading: boolean;
+    mensajeAlert: string;
+    estiloAlert: string;
     muestraMensajeToast: boolean;
     mensajeToast: string;
     tituloVistaPrevia: string;
-    p: number = 1;
-    itemsPerPage: number = 10;
+    p = 1;
+    itemsPerPage = 10;
     url: string;
     idEncuestaShare: number;
+    isDesktop: boolean;
+    isMobile: boolean;
 
-    encuesta = { 
-        estadoEncuesta:"" 
-    } 
- 
-    estados = [ 
-        { 
-            codigo: "CRE", 
-            descripcion: "creada" 
-        }, 
-        { 
-            codigo: "ARC", 
-            descripcion: "archivada" 
-        }, 
-        { 
-            codigo: "RES", 
-            descripcion: "respondida" 
-        }] 
-
-    constructor(private _surveyService: SurveyService, 
-                private router: Router,
-                private alertService: AlertService,
-                private route: ActivatedRoute,
-                routerAlert: Router){
-           this.loading = true;
-           this.alertService = alertService;
-           routerAlert = routerAlert;
+    encuesta = {
+        estadoEncuesta: ''
     }
 
-    /** 
-     * Inicializacion de pantalla: 
-     * Carga de encuestas por usuario  
-     * Seteo de encuesta como NO SELECCIONADA 
-     */ 
-    ngOnInit() { 
-        this.loadEncuestas(); 
-        this.encuestaActiva = false; 
-    } 
+    estados = [
+        {
+            codigo: 'CRE',
+            descripcion: 'creada'
+        },
+        {
+            codigo: 'ARC',
+            descripcion: 'archivada'
+        },
+        {
+            codigo: 'RES',
+            descripcion: 'respondida'
+        }]
 
-
-    /** 
-     * Cargar encuestas por usuarios 
-     */ 
-    loadEncuestas(){ 
-        this._surveyService.getEncuestas_x_Usuario(this.currentUser.idUsuario) 
-          .subscribe(resp => {
-            this.encuestas = resp;
-            this.loading = false;
-          }); 
-    } 
-
-    /** 
-     * Busqueda de encuestas por titulo 
-     */ 
-    buscarEncuestas(termino:string,estado:string,fecha:string){
-        console.log(termino+" "+estado+" "+fecha)
+    constructor(private _surveyService: SurveyService,
+        private router: Router,
+        private alertService: AlertService,
+        private route: ActivatedRoute,
+        private deviceService: DeviceDetectorService,
+        routerAlert: Router) {
         this.loading = true;
-        this.encuestas.splice(0,this.encuestas.length)
-        var codigoFiltro:string="";
-        if(termino==="" && estado===""){
-            this._surveyService.getEncuestas_x_Usuario(this.currentUser.idUsuario) 
+        this.alertService = alertService;
+        routerAlert = routerAlert;
+    }
+
+    /**
+     * Inicializacion de pantalla:
+     * Carga de encuestas por usuario
+     * Seteo de encuesta como NO SELECCIONADA
+     */
+    ngOnInit() {
+        this.isMobile = this.deviceService.isMobile();
+        this.isDesktop = this.deviceService.isDesktop();
+        this.loadEncuestas();
+        this.encuestaActiva = false;
+    }
+
+
+    /**
+     * Cargar encuestas por usuarios
+     */
+    loadEncuestas() {
+        this._surveyService.getEncuestas_x_Usuario(this.currentUser.idUsuario)
             .subscribe(resp => {
-              this.encuestas = resp;
-              this.loading = false;
-            }); 
-        }else{
-            if(termino!="" && estado != ""){codigoFiltro="1"} //busca por termino y estado
-            if(termino!="" && estado === ""){codigoFiltro="2"}//busca por termino
-            if(termino==="" && estado != ""){codigoFiltro="3"}//busca por estado
-        this._surveyService.getEncuestaPorFiltros(termino,estado,fecha,this.currentUser.idUsuario,codigoFiltro)
-            .subscribe( (resp:any) => {
-                console.log(resp)
                 this.encuestas = resp;
                 this.loading = false;
-                if(this.encuestas.length === 0){
-                    this.mensajeAlert = "No se registran encuestas con los filtros seleccionados."
-                    // this.estiloAlert = "alert-warning"
-                }
             });
+    }
+
+    /**
+     * Busqueda de encuestas por titulo
+     */
+    buscarEncuestas(termino: string, estado: string, fecha: string) {
+        console.log(termino + ' ' + estado + ' ' + fecha)
+        this.loading = true;
+        this.encuestas.splice(0, this.encuestas.length)
+        let codigoFiltro = '';
+        if (termino === '' && estado === '') {
+            this._surveyService.getEncuestas_x_Usuario(this.currentUser.idUsuario)
+                .subscribe(resp => {
+                    this.encuestas = resp;
+                    this.loading = false;
+                });
+        } else {
+            if (termino !== '' && estado !== '') { codigoFiltro = '1' } // busca por termino y estado
+            if (termino !== '' && estado === '') { codigoFiltro = '2' }// busca por termino
+            if (termino === '' && estado !== '') { codigoFiltro = '3' }// busca por estado
+            this._surveyService.getEncuestaPorFiltros(termino, estado, fecha, this.currentUser.idUsuario, codigoFiltro)
+                .subscribe((resp: any) => {
+                    console.log(resp)
+                    this.encuestas = resp;
+                    this.loading = false;
+                    if (this.encuestas.length === 0) {
+                        this.mensajeAlert = 'No se registran encuestas con los filtros seleccionados.'
+                        // this.estiloAlert = "alert-warning"
+                    }
+                });
         }
-    } 
+    }
 
     /**
      * Mostrar vista previa de la encuesta creada.
      * @param encuesta encuesta seleccionada en la grilla
      */
-    selectEncuesta(encuesta){
+    selectEncuesta(encuesta) {
         this.activeEncuesta = encuesta;
         this.encuestaActiva = true;
         console.log(this.activeEncuesta)
-        this.surveyRender = JSON.parse(this.activeEncuesta.definicionJSON) 
+        this.surveyRender = JSON.parse(this.activeEncuesta.definicionJSON)
         console.log(this.surveyRender)
         const surveyModel = new Survey.ReactSurveyModel(this.surveyRender);
 
         Survey.SurveyNG.render('surveyElement', { model: surveyModel });
 
-        surveyModel.onComplete.add(function(result) {
-            document.querySelector('#surveyResult').innerHTML = "result: " + JSON.stringify(result.data);
+        surveyModel.onComplete.add(function (result) {
+            document.querySelector('#surveyResult').innerHTML = 'result: ' + JSON.stringify(result.data);
             console.log(result.data)
-            });
+        });
     }
 
 
-    /** 
+    /**
      * Ejecuta evento al seleccionar un estado del combobox.
-     */ 
-    selectEstado(estadoEncuesta){
-        if (estadoEncuesta !== undefined)
-        {
-            for (var index = 0; index < this.estados.length; index++) 
-            {
-                if (this.estados[index].codigo === estadoEncuesta)
-                {
-                var descripcion = this.estados[index].descripcion;
+     */
+    selectEstado(estadoEncuesta) {
+        if (estadoEncuesta !== undefined) {
+            for (let index = 0; index < this.estados.length; index++) {
+                if (this.estados[index].codigo === estadoEncuesta) {
+                    // tslint:disable-next-line
+                    var descripcion = this.estados[index].descripcion;
                 }
-            } 
-            this._surveyService.getEncuestaByEstado(descripcion,this.currentUser.idUsuario) 
-            .subscribe(); 
+            }
+            this._surveyService.getEncuestaByEstado(descripcion, this.currentUser.idUsuario)
+                .subscribe();
         }
-    }    
+    }
 
-     /** 
-     * Mediante el servicio surveyService.archivarEncuesta permite modificar el estado de la encuesta a ARCHIVADA 
-     * @param idEncuesta id de Encuesta 
-     */ 
-    archivarEncuesta(idEncuesta,i){ 
-        var index = this.absoluteIndex(i);
-        let idUsuario = this.currentUser.idUsuario; 
-        if(this.encuestas[index].estadoEncuesta != "archivada"){
+    /**
+    * Mediante el servicio surveyService.archivarEncuesta permite modificar el estado de la encuesta a ARCHIVADA
+    * @param idEncuesta id de Encuesta
+    */
+    archivarEncuesta(idEncuesta, i) {
+        const index = this.absoluteIndex(i);
+        const idUsuario = this.currentUser.idUsuario;
+        if (this.encuestas[index].estadoEncuesta !== 'archivada') {
             this.alertService.confirm(
-            'Archivar encuesta.',
-            '¿ Desea archivar la encuesta "'+ this.encuestas[index].tituloEncuesta +'" ?',
-            () => {
-                this._surveyService.archivarEncuesta(idEncuesta,idUsuario) 
-                .subscribe( 
-                    res => { 
-                        this.encuestas[index].estadoEncuesta = "archivada";
-                    }
-                )
-            },
-            () => {/*no hay accion al cancelar*/},
-            'La encuesta ha sido archivada.');
-        }else{
-            this.alertService.warning("La encuesta ya está archivada.");
+                'Archivar encuesta.',
+                '¿ Desea archivar la encuesta "' + this.encuestas[index].tituloEncuesta + '" ?',
+                () => {
+                    this._surveyService.archivarEncuesta(idEncuesta, idUsuario)
+                        .subscribe(
+                            res => {
+                                this.encuestas[index].estadoEncuesta = 'archivada';
+                            }
+                        )
+                },
+                () => {/*no hay accion al cancelar*/ },
+                'La encuesta ha sido archivada.');
+        } else {
+            this.alertService.warning('La encuesta ya está archivada.');
         }
     }
 
@@ -188,12 +192,12 @@ export class misEncuestasComponent implements OnInit {
      * Redirije a pantalla de estadisticas de la encuesta seleccionada.
      * @param idEncuesta id de Encuesta
      */
-    verEstadisticas(idEncuesta,i){
-        var index = this.absoluteIndex(i);
-        if(this.encuestas[index].estadoEncuesta === "respondida"){
-            this.router.navigate(["dashboard/"+idEncuesta]); 
-        }else {
-            this.alertService.warning("La encuesta se encuentra en estado "+ this.encuestas[index].estadoEncuesta);
+    verEstadisticas(idEncuesta, i) {
+        const index = this.absoluteIndex(i);
+        if (this.encuestas[index].estadoEncuesta === 'respondida') {
+            this.router.navigate(['dashboard/' + idEncuesta]);
+        } else {
+            this.alertService.warning('La encuesta se encuentra en estado ' + this.encuestas[index].estadoEncuesta);
         }
     }
 
@@ -201,11 +205,11 @@ export class misEncuestasComponent implements OnInit {
      * Redirije a pantalla de respuesta de la encuesta seleccionada.
      * @param idEncuesta id de Encuesta
      */
-    responderEncuesta(idEncuesta,i){
-        var index = this.absoluteIndex(i);
+    responderEncuesta(idEncuesta, i) {
+        const index = this.absoluteIndex(i);
         this.idEncuestaShare = idEncuesta;
-        var ciphertext = CryptoJS.DES.encrypt(idEncuesta.toString(), 'Nurvey123');
-        this.url = 'https://nurvey-front-dev.herokuapp.com/respuesta/'+ciphertext.toString();
+        const ciphertext = CryptoJS.DES.encrypt(idEncuesta.toString(), 'Nurvey123');
+        this.url = 'https://nurvey-front-dev.herokuapp.com/respuesta/' + ciphertext.toString();
 
         // var bytes  = CryptoJS.Rabbit.decrypt(ciphertext.toString(), 'Nurvey123');
         // var plaintext = bytes.toString(CryptoJS.enc.Utf8);
@@ -223,49 +227,48 @@ export class misEncuestasComponent implements OnInit {
         // }
     }
 
-    share(idEncuesta){
-        this.router.navigate(["sharesurvey/"+idEncuesta]);
+    share(idEncuesta) {
+        this.router.navigate(['sharesurvey/' + idEncuesta]);
     }
 
     /**
-     * Redirije a pantalla de editor de encuesta con la encuesta seleccionada como plantilla base 
+     * Redirije a pantalla de editor de encuesta con la encuesta seleccionada como plantilla base
      * para modificar su contenido y crear una nueva encuesta
      * @param idEncuesta id de Encuesta
      */
-    clonarEncuesta(idEncuesta,i){
-        var index = this.absoluteIndex(i);
+    clonarEncuesta(idEncuesta, i) {
+        const index = this.absoluteIndex(i);
         this.alertService.confirm('Duplicado de encuesta.',
-        '¿ Desea duplicar la encuesta "'+ this.encuestas[index].tituloEncuesta +'" ?',
-        () => {this.router.navigate(["editor/"+idEncuesta]);},
-        () => {/*no hay accion al cancelar*/},
-        'Se ha duplicado la encuesta.');
+            '¿ Desea duplicar la encuesta "' + this.encuestas[index].tituloEncuesta + '" ?',
+            () => { this.router.navigate(['editor/' + idEncuesta]); },
+            () => {/*no hay accion al cancelar*/ },
+            'Se ha duplicado la encuesta.');
     }
 
-    vistaPrevia(encuesta){
+    vistaPrevia(encuesta) {
         console.log(encuesta)
         this.tituloVistaPrevia = encuesta.tituloEncuesta;
-        var editorOptions = {
-            showEmbededSurveyTab: false, 
+        const editorOptions = {
+            showEmbededSurveyTab: false,
             showJSONEditorTab: false,
             showApplyButtonInEditors: false
         };
-        this.testSurveyModel = new SurveyEditor.SurveyEditor('surveyContainerInPopup',editorOptions);
+        this.testSurveyModel = new SurveyEditor.SurveyEditor('surveyContainerInPopup', editorOptions);
         this.testSurveyModel.text = encuesta.definicionJSON;
         this.testSurveyModel.showTestSurvey();
-        var x = document.getElementsByClassName('nav-item');
-        for(var i=0, len=x.length; i<len; i++)
-            { x[i].remove(); }
+        const x = document.getElementsByClassName('nav-item');
+        for (let i = 0, len = x.length; i < len; i++) { x[i].remove(); }
     }
 
     absoluteIndex(indexOnPage: number): number {
         return this.itemsPerPage * (this.p - 1) + indexOnPage;
-      }
+    }
 
     copyClipboard() {
         /* Get the text field */
-        var copyText = document.getElementById("modalSurveySubtitle");
+        const copyText = document.getElementById('modalSurveySubtitle');
 
         /* Copy the text inside the text field */
-        document.execCommand("copy");
+        document.execCommand('copy');
     }
 }
